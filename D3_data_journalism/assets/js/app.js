36c9a99bfb956867e4351af92ac43aed69f27bfb
JeniLam <jenilam639@gmail.com>
2021-01-03 14:47:27 -0600

    Adds x/y params and linear scales

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -4,10 +4,10 @@ var svgHeight = 600;
 
 // Define the chart's margins as an object
 var margin = {
-    top: 60,
-    right: 60,
-    bottom: 60,
-    left: 60
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
 };
 
 // Define dimensions of the chart area
@@ -23,6 +23,36 @@ var svg = d3.select("#scatter")
 // Append a group area, then set its margins
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label - taken from Day 3 activity 12
+function xScale(scatterData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(scatterData, d => d[chosenXAxis]) * 0.8,
+        d3.max(scatterData, d => d[chosenXAxis]) * 1.2
+      ])
+      .range([0, chartWidth]);
+  
+    return xLinearScale;
+  
+  };
+
+  // function used for updating y-scale var upon click on axis label
+function YScale(scatterData, chosenYAxis) {
+    // create scales
+    var YLinearScale = d3.scaleLinear()
+      .domain([d3.min(scatterData, d => d[chosenYAxis]) * 0.8,
+        d3.max(scatterData, d => d[chosenYAxis]) * 1.2
+      ])
+      .range([chartHeight, o]);
+  
+    return yLinearScale;
+  
+  }
 //Read in csv to look at data and how it is arranged
 
 d3.csv("assets/data/data.csv").then(function (scatterData) {
