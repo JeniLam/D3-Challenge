239eab70aa4cc9fc16c18cf2da9f5d4989ec71d9
JeniLam <jenilam639@gmail.com>
2021-01-02 21:14:44 -0600

    Adds boiler plate code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,29 @@
-// @TODO: YOUR CODE HERE!
+// Add boiler plate inforamtion for SVG area, margins, etc
+var svgWidth = 960;
+var svgHeight = 500;
 
+// Define the chart's margins as an object
+var margin = {
+    top: 60,
+    right: 60,
+    bottom: 60,
+    left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Select scatter id to hold chart, append SVG area to it, and set its dimensions
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 //Read in csv to look at data and how it is arranged
 
-d3.csv("assets/data/data.csv").then(function(scatterData){
+d3.csv("assets/data/data.csv").then(function (scatterData) {
     console.log(scatterData)
 })
